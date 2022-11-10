409e4f3f083267bbc478b25b7916a2952f3876d2
Ngo-Tuan-Kiet <tuankietngo2002@outlook.de>
2022-11-10 16:33:08 +0100

    First experiment

--- src/main/java/exercise1/TPMMSJava.java
+++ src/main/java/exercise1/TPMMSJava.java
@@ -1,13 +1,17 @@
 package exercise1;
 
 import de.hpi.dbs2.ChosenImplementation;
-import de.hpi.dbs2.dbms.BlockManager;
-import de.hpi.dbs2.dbms.BlockOutput;
-import de.hpi.dbs2.dbms.Relation;
+import de.hpi.dbs2.dbms.*;
 import de.hpi.dbs2.exercise1.SortOperation;
 import org.jetbrains.annotations.NotNull;
 
-@ChosenImplementation(false)
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@ChosenImplementation(true)
 public class TPMMSJava extends SortOperation {
     public TPMMSJava(@NotNull BlockManager manager, int sortColumnIndex) {
         super(manager, sortColumnIndex);
@@ -15,11 +19,42 @@ public class TPMMSJava extends SortOperation {
 
     @Override
     public int estimatedIOCost(@NotNull Relation relation) {
-        throw new UnsupportedOperationException("TODO");
+
+        System.out.println("erqewljrlkqwjr");
+        return 1;
+        //throw new UnsupportedOperationException("TODO");
     }
 
     @Override
     public void sort(@NotNull Relation relation, @NotNull BlockOutput output) {
-        throw new UnsupportedOperationException("TODO");
+        BlockManager bm = getBlockManager();
+        Iterator<Block> r_it = relation.iterator();
+
+
+        System.out.println(bm);
+        int freeBlocks = bm.getFreeBlocks();
+
+/*        Block test1 = r_it.next();
+
+        bm.load(test1);
+        System.out.println(bm);
+        System.out.println(test1.iterator().next());*/
+        for (int i = 0; i < freeBlocks; i++) {
+            Block b = r_it.next();
+            bm.load(b);
+            int block_size = b.getSize();
+            List<Tuple> tuples = new ArrayList<>();
+
+            for (int j = 0; j < block_size; j++) {
+                Tuple t = b.iterator().next();
+                System.out.println(t.get(0).getClass());
+                tuples.add(t);
+               /* List result = tuples.stream()
+                        .sorted((o1,o2)-> o1.get(0).compareTo(o2.get(0)))
+                        .collect(Collectors.toList());*/
+            }
+
+        }
+        //throw new UnsupportedOperationException("TODO");
     }
 }
