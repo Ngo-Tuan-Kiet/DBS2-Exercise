43985b8904f41fb85516b2aadea198057f2ab4bd
Zohka <66786380+Zohka@users.noreply.github.com>
2022-11-16 01:37:45 +0100

    dlkgj

--- src/test/kotlin/exercise1/TPMMSExerciseTests.kt
+++ src/test/kotlin/exercise1/TPMMSExerciseTests.kt
@@ -46,21 +46,6 @@ class TPMMSExerciseTests {
                 TPMMSExerciseTests::class.java.getResourceAsStream("sorted_by_col0.output.csv")!!,
             )
             assertEquals(controlRelation.joinToString(), outputRelation.joinToString())
-            val control_it = controlRelation.iterator()
-            val output_it = outputRelation.iterator()
-
-            val cfirst_block = blockManager.load(control_it.next())
-            val ofirst_block = blockManager.load(output_it.next())
-
-            assertEquals(cfirst_block.get(0).get(0), ofirst_block.get(0).get(0))
-
-            blockManager.release(cfirst_block, false)
-            blockManager.release(ofirst_block, false)
-
-            val csecond_block = blockManager.load(control_it.next())
-            val osecond_block = blockManager.load(output_it.next())
-
-            assertEquals(csecond_block.get(0).get(0), osecond_block.get(0).get(0))
 
             assertEquals(3*6, cost.ioCost)
         }
@@ -101,6 +86,55 @@ class TPMMSExerciseTests {
             assertEquals(controlRelation.joinToString(), outputRelation.joinToString())
 
             assertEquals(3*6, cost.ioCost)
+        }
+    }
+    @Test
+    fun `TPMMS sorts test file by column 0 and checks if the 0th column of the first tuple of first two blocks are the same`() {
+        val columnDefinition = ColumnDefinition(
+                ColumnDefinition.ColumnType.INTEGER,
+                ColumnDefinition.ColumnType.STRING,
+                ColumnDefinition.ColumnType.DOUBLE,
+        )
+
+        with(DBMS(
+                totalBlocks = 3,
+                blockCapacity = 2
+        )) {
+            val inputRelation = loadRelation(
+                    blockManager, columnDefinition,
+                    TPMMSExerciseTests::class.java.getResourceAsStream("input.csv")!!,
+            )
+            val outputRelation = createOutputRelation(
+                    blockManager, columnDefinition
+            )
+
+            val cost = trackIOCost {
+                val sortOperation = getImplementation(blockManager, 0)
+
+                assert(blockManager.usedBlocks == 0)
+                sortOperation.execute(inputRelation, outputRelation)
+                assert(blockManager.usedBlocks == 0)
+            }
+
+            val controlRelation = loadRelation(
+                    blockManager, columnDefinition,
+                    TPMMSExerciseTests::class.java.getResourceAsStream("sorted_by_col0.output.csv")!!,
+            )
+            val control_it = controlRelation.iterator()
+            val output_it = outputRelation.iterator()
+
+            val cfirst_block = blockManager.load(control_it.next())
+            val ofirst_block = blockManager.load(output_it.next())
+
+            assertEquals(cfirst_block.get(0).get(0), ofirst_block.get(0).get(0))
+
+            blockManager.release(cfirst_block, false)
+            blockManager.release(ofirst_block, false)
+
+            val csecond_block = blockManager.load(control_it.next())
+            val osecond_block = blockManager.load(output_it.next())
+
+            assertEquals(csecond_block.get(0).get(0), osecond_block.get(0).get(0))
         }
     }
 
